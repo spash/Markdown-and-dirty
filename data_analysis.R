d0d9fba3ef053ce3586e3575218c7c7399cdb533
SJ <shivani@D-10-18-182-190.dhcp4.washington.edu>
2019-03-13 11:54:49 -0700

    update

--- data_analysis.R
+++ data_analysis.R
@@ -17,10 +17,26 @@ health_inspection_range <- range(reviews_Vs_safety$Inspection.Score)
 
 #stephen
 price_vs_health <- combined_df %>% 
-  select(c(price_range, Grade, location.zipcode))
-#stephen ggplot
-#ggplot(data = combined_df) +
- # aes(x = Grade, y = price_range) +
-#  geom_jitter(alpha = .5, height = 0, width = .25) +
- # labs(title = "Food Safety vs Price") +
-#theme_light()
+  select(c(name, average_cost_for_two, Grade, location.zipcode))
+
+
+#stephen_response <- p("")
+
+
+shivani_response <- p(" The plot above shows customer reviews (out of 5) plotted against health inspection ratings. The purpose of this is to 
+                      answer the following questions: 
+                      Do customers care about health inspections when they go to restaurants?
+                      Do they look at the hygenic conditions of the restaurant where the food is made or do they only care about the taste of the food?
+                      Looking at the data above, it seems that for the most part, restaurants with higher food ratings seem to have lower inspection scores")
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ my_server.R
@@ -0,0 +1,56 @@
+source("data_analysis.R")
+
+
+my_server <- function(input, output) {
+  output$health_plot <- renderPlot({
+    filtered_inspection <- reviews_Vs_safety %>%
+      filter(location.zipcode == input$zip)
+    
+    inspection_graph <- ggplot(data = filtered_inspection, aes(x = name, y = Inspection.Score, fill = location.zipcode)) +
+      geom_bar(stat = "identity") +
+      coord_flip() +
+      labs(
+        title = "Health inspection score for Restaurants in Seattle",
+        x = "Restaurant Name",
+        y = "Inspection Score",
+        color = "zip code"
+      )
+    
+    inspection_graph
+  })
+  
+  output$review_plot <- renderPlot({
+    filtered_review <- reviews_Vs_safety %>% 
+      filter(user_rating.aggregate_rating > input$review[1] & user_rating.aggregate_rating < input$review[2]) %>% 
+      filter(Inspection.Score > input$inspection[1] & Inspection.Score < input$inspection[2])
+    
+    
+    review_graph <- ggplot(data = filtered_review) +
+      geom_point(mapping = aes(x = Inspection.Score, y = user_rating.aggregate_rating, color = location.zipcode)) +
+      labs(
+        title = "Customer reviews vs. health inspection score",
+        x = "Inspection Score",
+        y = "Customer review (out of 5)",
+        color = "zip code"
+      )
+    
+    review_graph
+    
+  })
+  
+  output$price_plot <- renderPlot({
+    
+    filtered_price <- price_vs_health %>% 
+      filter(location.zipcode == input$zips)
+    
+    price_graph<- ggplot(data = filtered_price) +
+      aes(x = Grade, y = average_cost_for_two) +
+      geom_jitter(alpha = .5, height = 0, width = .25) +
+    labs(title = "Food Safety vs Price",
+         x = "Food safety grade (lower is safer)",
+         y = "average cost of food for two") +
+      theme_light()
+    
+    price_graph
+  })
+}
